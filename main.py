775e976b3cf62550554a9e7810f72c6bba71c9fd
Ijas7 <82671424+Ijas7@users.noreply.github.com>
2022-08-25 13:40:47 +0530

    decimal update

--- main.py
+++ main.py
@@ -87,7 +87,9 @@ with tab2:
 			
 	st.markdown('###### Leaderboard - Block wise')
 
-	st.dataframe(df.groupby(['Block']).round(sum()['Point'].reset_index().sort_values(by='Point', ascending=False),1))
+	st.dataframe(df.groupby(['Block']).sum()['Point'].reset_index().sort_values(by='Point', ascending=False))
+	
+	st.dataframe(df.style.format({"Point": "{:.2f}"}))
 		
 		
 
